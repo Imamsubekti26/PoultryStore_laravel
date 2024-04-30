96e0fdf7fc573bb5ec6fc11d2aff4bfd1dd133e0
Imamsubekti26 <imam0subekti@gmail.com>
2024-04-30 11:39:37 +0700

    create CRUD for partner

--- app/Http/Controllers/partnerController.php
+++ app/Http/Controllers/partnerController.php
@@ -2,30 +2,113 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\GenerateResponse;
+use App\Models\Partner;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class PartnerController extends Controller
 {
-    public function index()
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
     {
-        $data['title'] = 'Mitra';
-        $data['selected_menu'] = 'partner';
-        return view('partner.main', $data);
+        $this->setTitle('mitra', 'partner');
     }
 
-    public function create()
+    /**
+     * Send batch of data.
+     */
+    public function list(Request $request): JsonResponse
     {
-        $data['do'] = 'Tambah';
-        $data['title'] = 'Mitra';
-        $data['selected_menu'] = 'partner';
-        return view('partner.form', $data);
+        try {
+            $data = Partner::all();            
+            return response()->json(GenerateResponse::success($data));
+        } catch (\Throwable $th) {            
+            return response()->json(GenerateResponse::failed("failed to getting data: $th"), 500);
+        }
     }
 
-    public function edit(string $id)
+    /**
+     * Send a single data depend on id.
+     */
+    public function show(string $id): JsonResponse
     {
-        $data['do'] = 'Edit';
-        $data['title'] = 'Mitra';
-        $data['selected_menu'] = 'partner';
-        return view('partner.form', $data);
+        try {
+            $data = Partner::find($id);
+            if(is_null($data)){
+                throw new Exception('data not found');
+            }
+            return response()->json(GenerateResponse::success($data));
+        } catch (\Throwable $th) {
+            $error_code = $th == 'data not found' ? 404 : 500 ;
+            return response()->json(GenerateResponse::failed("failed to getting data: $th"), $error_code);
+        }
+    }
+
+    /**
+     * Store data to database.
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $form = [
+            "name" => $request->name,
+            "address" => $request->address,
+            "area" => $request->area,
+            "phone" => $request->phone,
+            "partnership_type" => $request->partnership_type
+        ];
+
+        try {
+            $result = Partner::create($form);
+            if(is_null($result)){
+                throw new Exception('null');
+            }
+            return response()->json(GenerateResponse::success($result), 201);
+        } catch (\Throwable $th) {            
+            return response()->json(GenerateResponse::failed("failed to adding data: $th"), 500);
+        }
+    }
+
+    /**
+     * Update data in database depend on id.
+     */
+    public function update(Request $request, string $id): JsonResponse
+    {
+        $form = [
+            "name" => $request->name,
+            "address" => $request->address,
+            "area" => $request->area,
+            "phone" => $request->phone,
+            "partnership_type" => $request->partnership_type
+        ];
+
+        try {
+            $result = Partner::whereKey($id)->update($form);
+            if($result == 0){
+                throw new Exception('null');
+            }
+            return response()->json(GenerateResponse::success(['affected_row' => $result]));
+        } catch (\Throwable $th) {            
+            return response()->json(GenerateResponse::failed("failed to updating data: $th"), 500);
+        }
+    }
+
+    /**
+     * Delete data from database depend on id.
+     */
+    public function destroy(string $id): JsonResponse
+    {
+        try {
+            $result = Partner::destroy($id);
+            if($result == 0) {
+                throw new Exception('failed to remove data with id '.$id);
+            }
+            return response()->json(GenerateResponse::success(['affected_row' => $result]));
+        } catch (\Throwable $th) {            
+            return response()->json(GenerateResponse::failed("failed to remove data: $th"), 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CarController;
+use App\Http\Controllers\PartnerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,9 @@ Route::get('/car/{id}', [CarController::class, 'show']);
 Route::post('/car', [CarController::class, 'store']);
 Route::put('/car/{id}', [CarController::class, 'update']);
 Route::delete('/car/{id}', [CarController::class, 'destroy']);
+
+Route::get('/partner', [PartnerController::class, 'list']);
+Route::get('/partner/{id}', [PartnerController::class, 'show']);
+Route::post('/partner', [PartnerController::class, 'store']);
+Route::put('/partner/{id}', [PartnerController::class, 'update']);
+Route::delete('/partner/{id}', [PartnerController::class, 'destroy']);
